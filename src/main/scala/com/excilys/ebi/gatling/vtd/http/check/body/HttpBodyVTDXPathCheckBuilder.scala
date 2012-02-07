620d15fcf8591dd82c50397d421d853e92b10e8b
Stephane Landelle <slandelle+github@excilys.com>
2012-02-07 23:53:47 +0100

    Mutualize code between HttpBodyCheckBuilders

--- src/main/scala/com/excilys/ebi/gatling/vtd/http/check/body/HttpBodyVTDXPathCheckBuilder.scala
+++ src/main/scala/com/excilys/ebi/gatling/vtd/http/check/body/HttpBodyVTDXPathCheckBuilder.scala
@@ -16,40 +16,23 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 package com.excilys.ebi.gatling.vtd.http.check.body
-
 import com.excilys.ebi.gatling.core.check.CheckContext.{ setAndReturnCheckContextAttribute, getCheckContextAttribute }
-import com.excilys.ebi.gatling.core.check.{ CheckOneBuilder, CheckMultipleBuilder }
 import com.excilys.ebi.gatling.core.session.Session
-import com.excilys.ebi.gatling.http.check.HttpMultipleCheckBuilder
-import com.excilys.ebi.gatling.http.request.HttpPhase.CompletePageReceived
+import com.excilys.ebi.gatling.http.check.body.HttpBodyCheckBuilder
 import com.excilys.ebi.gatling.vtd.check.extractor.VTDXPathExtractor
 import com.ning.http.client.Response
 
-import HttpBodyVTDXPathCheckBuilder.HTTP_BODY_VTD_XPATH_EXTRACTOR_CONTEXT_KEY
-
 object HttpBodyVTDXPathCheckBuilder {
 
-	val HTTP_BODY_VTD_XPATH_EXTRACTOR_CONTEXT_KEY = "HttpBodyVTDXPathExtractor"
+	def vtdXpath(expression: Session => String) = new HttpBodyCheckBuilder(findExtractorFactory, findAllExtractoryFactory, countExtractoryFactory, expression)
 
-	def vtdXpath(expression: Session => String) = new HttpBodyVTDXPathCheckBuilder(expression)
-}
+	private val HTTP_BODY_VTD_XPATH_EXTRACTOR_CONTEXT_KEY = "HttpBodyVTDXPathExtractor"
 
-/**
- * This class builds a response body check based on regular expressions
- *
- * @param expression the function returning the expression representing expression is to be checked
- */
-class HttpBodyVTDXPathCheckBuilder(expression: Session => String) extends HttpMultipleCheckBuilder[String](expression, CompletePageReceived) {
-
-	def find = find(0)
-
-	def getCachedExtractor(response: Response) = getCheckContextAttribute(HTTP_BODY_VTD_XPATH_EXTRACTOR_CONTEXT_KEY).getOrElse {
+	private def getCachedExtractor(response: Response) = getCheckContextAttribute(HTTP_BODY_VTD_XPATH_EXTRACTOR_CONTEXT_KEY).getOrElse {
 		setAndReturnCheckContextAttribute(HTTP_BODY_VTD_XPATH_EXTRACTOR_CONTEXT_KEY, new VTDXPathExtractor(response.getResponseBodyAsBytes))
 	}
 
-	def find(occurrence: Int) = new CheckOneBuilder(checkBuildFunction, (response: Response) => getCachedExtractor(response).extractOne(occurrence))
-
-	def findAll = new CheckMultipleBuilder(checkBuildFunction, (response: Response) => getCachedExtractor(response).extractMultiple)
-
-	def count = new CheckOneBuilder(checkBuildFunction, (response: Response) => getCachedExtractor(response).count)
+	private def findExtractorFactory(occurrence: Int) = (response: Response) => getCachedExtractor(response).extractOne(occurrence)(_)
+	private val findAllExtractoryFactory = (response: Response) => getCachedExtractor(response).extractMultiple(_)
+	private val countExtractoryFactory = (response: Response) => getCachedExtractor(response).count(_)
 }
